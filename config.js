23c2de123c42f1aadcc71ab6937b8db9d5ef3688
KinmenRay <31943819+juifa007@users.noreply.github.com>
2026-09-07 09:49:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 只填入教師登入服務網址；帳號、密碼、密鑰不得填在本檔案。
-https://script.google.com/macros/s/AKfycbwkqiZA3_e48dL-ORDoZ1XppEwcjQPJw9DvUsterAOGJGqhrOTQClHrDq6gIZupGrYu/exec
+https://script.google.com/macros/s/AKfycbw0mO2sAsvZPPavBVfWQ2czqLk52HRM_x86slw580MZwlO547Y6SM65hPuLvxjWkMAW/exec
