f31821befec8c47f0c9e235098c61b79c0cc8cb5
KinmenRay <31943819+juifa007@users.noreply.github.com>
2026-09-07 09:57:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 只填入教師登入服務網址；帳號、密碼、密鑰不得填在本檔案。
-https://script.google.com/macros/s/AKfycbxmgSPzvbrLwajtYqNZSgMLFRT686KKnPpc5g6jW4osEVsW46VKWxvneA3aL7iu0m5Y/exec
+window.SITE_CONFIG = { teacherApiUrl: "https://script.google.com/macros/s/AKfycbxmgSPzvbrLwajtYqNZSgMLFRT686KKnPpc5g6jW4osEVsW46VKWxvneA3aL7iu0m5Y/exec" };
